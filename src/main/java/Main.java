fa29192c496b81b76fcd83c5a011c0348e2da8b7
kou <contact@awesome-linus.com>
2021-06-06 20:11:12 +0900

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import org.joda.time.LocalTime;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        LocalTime currentTime = new LocalTime();
+        System.out.println("The current local time is: " + currentTime);
+    }
+}
